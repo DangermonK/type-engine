5ee242915a46d11f4da434c5ff4df296ae8de354
Robin Hefter <r.hefter@ltur.de>
2022-04-16 14:52:33 +0200

    remove script disabling

--- src/core/container/Container.ts
+++ src/core/container/Container.ts
@@ -6,11 +6,9 @@ import { ObjectMap } from "../../scratch-engine/utils/ObjectMap";
 export class Container<Type extends Scriptable<any>> implements IContainer<Type>, IDisposable {
 
     private readonly _enabledScriptMap: ObjectMap<Type>;
-    private readonly _disabledScriptMap: ObjectMap<Type>;
 
     constructor() {
         this._enabledScriptMap = new ObjectMap<Type>();
-        this._disabledScriptMap = new ObjectMap<Type>();
     }
 
     protected get scripts(): Array<Type> {
@@ -39,22 +37,6 @@ export class Container<Type extends Scriptable<any>> implements IContainer<Type>
 
     hasElement<Element extends Type>(element: Element ): boolean {
         return this._enabledScriptMap.hasElement(element);
-    }
-
-    disableElement<Element extends Type>(element: { new(...args: any): Element }): void {
-        const el = this._enabledScriptMap.get(element);
-        if(el) {
-            this._enabledScriptMap.remove(el);
-            this._disabledScriptMap.set(el);
-        }
-    }
-
-    enableElement<Element extends Type>(element: { new(...args: any): Element }): void {
-        const el = this._disabledScriptMap.get(element);
-        if(el) {
-            this._disabledScriptMap.remove(el);
-            this._enabledScriptMap.set(el);
-        }
     }
 
     initialize(): void {
