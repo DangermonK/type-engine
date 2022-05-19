a9c9e1f220d64e8ccc0c9cb109ff7c783b4905df
Robin Hefter <r.hefter@ltur.de>
2022-05-19 19:27:47 +0200

    refactoring

--- src/scratch-engine/scene-scripts/GraphicsHandler.ts
+++ src/scratch-engine/scene-scripts/GraphicsHandler.ts
@@ -9,26 +9,24 @@ export class GraphicsHandler extends ScratchSceneScript {
 
     private _ctx?: CanvasRenderingContext2D;
 
-    private readonly _renderedEntities: Array<string>;
+    private readonly _renderedEntities: Set<string>;
     private readonly _entityHandler: EntityHandler;
 
     constructor(scene: ScratchScene) {
         super(scene);
 
         this._entityHandler = this.container.requireType(EntityHandler);
-        this._renderedEntities = new Array<string>();
+        this._renderedEntities = new Set<string>();
     }
 
     addRenderEntity(entity: ScratchEntity): void {
         if(entity.hasListener('render')) {
-            this._renderedEntities.push(entity.id);
+            this._renderedEntities.add(entity.id);
         }
     }
 
     removeRenderEntity(entity: ScratchEntity): void {
-        const index = this._renderedEntities.indexOf(entity.id);
-        if(index !== -1)
-            this._renderedEntities.splice(index, 1);
+        this._renderedEntities.delete(entity.id);
     }
 
     setCanvasRenderingContext(ctx: CanvasRenderingContext2D): void {
@@ -37,7 +35,7 @@ export class GraphicsHandler extends ScratchSceneScript {
         this.update = () => {
             this._ctx!.clearRect(0, 0, this._ctx!.canvas.width, this._ctx!.canvas.height);
 
-            for(const entity of this._entityHandler.getEntities(this._renderedEntities.values())) {
+            for(const entity of this._entityHandler.getEntities(this._renderedEntities)) {
                 entity.emit('render', this._ctx);
             }
         };
